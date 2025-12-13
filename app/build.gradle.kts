ff7a754251f8f1628a2f987bde3e9bad920a2dc7
Elsayed Magdy <saikoo1239@gmail.com>
2025-12-13 21:32:29 +0200

    feat: Add splash screen and update app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,6 +54,7 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.crashlytics)
     implementation(libs.firebase.perf)
+    implementation("androidx.core:core-splashscreen:1.0.1")
     val nav_version = "2.9.6"
     implementation("androidx.navigation:navigation-compose:$nav_version")
     implementation("androidx.navigation:navigation-runtime-ktx:$nav_version")
