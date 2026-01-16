ba5b1a2f9d8a26eafc7a297098f9dd9e1e020c53
Elsayed Magdy <saikoo1239@gmail.com>
2026-01-16 16:55:21 +0200

    feat: Add Ayah and Surah data models

---
+++ app/src/main/java/dev/sayed/mehrabalmomen/domain/entity/Ayah.kt
@@ -0,0 +1,9 @@
+package dev.sayed.mehrabalmomen.domain.entity
+
+data class Ayah(
+    val id: Int,
+    val ayahNumber: Int,
+    val juzNumber: Int,
+    val surahNumber: Int,
+    val text: String
+)
