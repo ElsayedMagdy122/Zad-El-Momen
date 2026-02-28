7f6007357542040366a0190e7355a36cd8ec4056
Elsayed Magdy <saikoo1239@gmail.com>
2026-02-28 21:49:49 +0200

    feat: Implement error handling and exception mapping

---
+++ app/src/main/java/dev/sayed/mehrabalmomen/data/util/SafeCall.kt
@@ -0,0 +1,18 @@
+package dev.sayed.mehrabalmomen.data.util
+
+import dev.sayed.mehrabalmomen.data.mappers.toAppException
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+
+suspend fun <T> safeCall(block: suspend () -> T): T {
+    return try {
+        block()
+    } catch (e: Throwable) {
+        throw e.toAppException()
+    }
+}
+
+fun <T> Flow<T>.safeFlow(): Flow<T> =
+    catch { e ->
+        throw e.toAppException()
+    }
