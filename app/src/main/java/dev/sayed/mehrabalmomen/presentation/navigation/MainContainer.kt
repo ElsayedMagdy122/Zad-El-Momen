06cd86764cc6ce3237179a8504bb611e9cd9db69
Elsayed Magdy <saikoo1239@gmail.com>
2026-02-28 21:50:05 +0200

    feat: localization and cleanup of navigation items

--- app/src/main/java/dev/sayed/mehrabalmomen/presentation/navigation/MainContainer.kt
+++ app/src/main/java/dev/sayed/mehrabalmomen/presentation/navigation/MainContainer.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
@@ -57,17 +58,7 @@ fun MainContainer(
             unselectedIcon = painterResource(R.drawable.ic_prayer_times_not_selected)
         ),
         NavItem(
-            title = "Radio",
-            selectedIcon = painterResource(R.drawable.ic_radio_selected),
-            unselectedIcon = painterResource(R.drawable.ic_radio_not_selected)
-        ),
-        NavItem(
-            title = "Radio",
-            selectedIcon = painterResource(R.drawable.ic_radio_selected),
-            unselectedIcon = painterResource(R.drawable.ic_radio_not_selected)
-        ),
-        NavItem(
-            title = "Radio",
+            title = localizedString(R.string.quran_radio),
             selectedIcon = painterResource(R.drawable.ic_radio_selected),
             unselectedIcon = painterResource(R.drawable.ic_radio_not_selected)
         )
@@ -105,12 +96,6 @@ fun MainContainer(
 
             composable<Route.RadioScreen> {
                 RadioScreen(rootNavController)
-            }
-            composable<Route.QiblahScreen> {
-                QiblahScreen(rootNavController)
-            }
-            composable<Route.QiblahScreen> {
-                QiblahScreen(rootNavController)
             }
         }
 
