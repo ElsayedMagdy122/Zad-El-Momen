21954060b785f2e378589fff14a0c0b5af76ae7c
Elsayed Magdy <saikoo1239@gmail.com>
2026-02-28 23:37:16 +0200

    refactor: rename ContinueTilawah to ReadingProgress

---
+++ app/src/main/java/dev/sayed/mehrabalmomen/domain/entity/quran/ReadingProgress.kt
@@ -0,0 +1,6 @@
+package dev.sayed.mehrabalmomen.domain.entity.quran
+
+data class ReadingProgress(
+    val surahId: Int,
+    val ayahId: Int
+)
--- app/src/main/java/dev/sayed/mehrabalmomen/domain/model/ContinueTilawah.kt
+++
@@ -1,6 +0,0 @@
-package dev.sayed.mehrabalmomen.domain.model
-
-data class ContinueTilawah(
-    val surahId: Int,
-    val ayahId: Int
-)
