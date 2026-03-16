21944def6872d4915e545ad21dff6759e9171cbd
Elsayed Magdy <saikoo1239@gmail.com>
2026-03-16 03:13:34 +0200

    build: bump versionCode to 11 and versionName to 1.0.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,8 +33,8 @@ android {
         applicationId = "dev.sayed.mehrabalmomen"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.0"
+        versionCode = 11
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
