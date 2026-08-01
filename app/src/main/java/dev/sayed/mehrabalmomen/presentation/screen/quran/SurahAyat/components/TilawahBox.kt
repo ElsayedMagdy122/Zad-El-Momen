924eb6285318b4240ce9161aed178bb0320e492e
Elsayed Magdy <saikoo1239@gmail.com>
2026-08-01 16:31:02 +0300

    feat: add loading state and dynamic data support to `TilawahBox` component

--- app/src/main/java/dev/sayed/mehrabalmomen/presentation/screen/quran/SurahAyat/components/TilawahBox.kt
+++ app/src/main/java/dev/sayed/mehrabalmomen/presentation/screen/quran/SurahAyat/components/TilawahBox.kt
@@ -3,13 +3,16 @@ package dev.sayed.mehrabalmomen.presentation.screen.quran.SurahAyat.components
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,137 +26,25 @@ import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.shadow.Shadow
 import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import dev.sayed.mehrabalmomen.R
-import dev.sayed.mehrabalmomen.design_system.theme.MehrabTheme
 import dev.sayed.mehrabalmomen.design_system.theme.Theme
+import dev.sayed.mehrabalmomen.presentation.base.LocalAppLocale
+import dev.sayed.mehrabalmomen.presentation.base.localizedString
+import dev.sayed.mehrabalmomen.presentation.base.toLocalizedDigits
 
-@Composable
-fun TilawahBox1(modifier: Modifier = Modifier) {
-    val isRtl = LocalLayoutDirection.current == LayoutDirection.Rtl
-    Column(
-        modifier = modifier
-            .widthIn(max = 400.dp)
-            .clip(RoundedCornerShape(12.dp))
-    ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .background(Theme.color.surfaces.surfaceLow)
-                .padding(vertical = 6.dp, horizontal = 12.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
-            Text(
-                text = "Maytham Al-Tammar",
-                color = Theme.color.primary.shadePrimary,
-                style = Theme.textStyle.label.small,
-                textAlign = TextAlign.Start,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Spacer(modifier = Modifier.weight(1f))
-            Icon(
-                painter = painterResource(R.drawable.ic_close),
-                tint = Theme.color.primary.primary,
-                contentDescription = null
-            )
-        }
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                // radius = blur in figma
-                .dropShadow(
-                    shape = RectangleShape, shadow = Shadow(
-                        radius = 12.dp,
-                        offset = DpOffset(x = 0.dp, y = 4.dp),
-                        color = Color.Black.copy(alpha = 0.04f),
-                    )
-                )
-                .dropShadow(
-                    shape = RectangleShape, shadow = Shadow(
-                        radius = 8.dp,
-                        offset = DpOffset(x = 0.dp, y = -2.dp),
-                        color = Color.Black.copy(alpha = 0.06f),
-                    )
-                )
-                .background(Theme.color.surfaces.surfaceLow)
-                .padding(horizontal = 8.dp, vertical = 12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Text(
-                text = "Al-Baarah: 4 aya",
-                color = Theme.color.secondary.shadeSecondary,
-                style = Theme.textStyle.label.small,
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                overflow = TextOverflow.Ellipsis
-            )
-            Row(
-                modifier = Modifier.padding(top = 12.dp),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.spacedBy(10.dp)
-            ) {
-                Icon(
-                    painter = painterResource(R.drawable.ic_user_list),
-                    contentDescription = null,
-                    tint = Theme.color.primary.primary
-                )
-                Row(
-                    modifier = Modifier
-                        .padding(start = 12.dp)
-                        .clip(RoundedCornerShape(8.dp))
-                        .background(Theme.color.surfaces.surface)
-                        .padding(vertical = 4.dp, horizontal = 12.dp),
-                ) {
-                    Icon(
-                        modifier = Modifier.graphicsLayer {
-                            scaleX = if (isRtl) -1f else 1f
-                        },
-                        painter = painterResource(R.drawable.ic_backward_02),
-                        contentDescription = null,
-                        tint = Theme.color.primary.primary
-                    )
-                    Icon(
-                        modifier = Modifier.padding(horizontal = 32.dp),
-                        painter = painterResource(R.drawable.ic_pause),
-                        contentDescription = null,
-                        tint = Theme.color.primary.primary
-                    )
-                    Icon(
-                        modifier = Modifier.graphicsLayer {
-                            scaleX = if (isRtl) -1f else 1f
-                        },
-                        painter = painterResource(R.drawable.ic_forward_02),
-                        contentDescription = null,
-                        tint = Theme.color.primary.primary
-                    )
-                }
-                Icon(
-                    painter = painterResource(R.drawable.ic_repeat_one_01),
-                    contentDescription = null,
-                    tint = Theme.color.primary.primary
-                )
-                Icon(
-                    modifier = Modifier.graphicsLayer {
-                        scaleX = if (isRtl) -1f else 1f
-                    },
-                    painter = painterResource(R.drawable.ic_curvy_right_direction),
-                    contentDescription = null,
-                    tint = Theme.color.primary.primary
-                )
-            }
-        }
-    }
-
-}
 @Composable
 fun TilawahBox(
+    readerName: String,
+    surahName: String,
+    currentAyahNumber: Int,
     isPlaying: Boolean,
+    isLoading: Boolean,
+    repeatCount: Int,
+    isContinuous: Boolean,
     modifier: Modifier = Modifier,
     onCloseClick: () -> Unit = {},
     onRecitersClick: () -> Unit = {},
@@ -161,7 +52,7 @@ fun TilawahBox(
     onPlayPauseClick: () -> Unit = {},
     onForwardClick: () -> Unit = {},
     onRepeatClick: () -> Unit = {},
-    onPlaylistClick: () -> Unit = {},
+    onContinuousClick: () -> Unit = {},
 ) {
     Column(
         modifier = modifier
@@ -169,22 +60,30 @@ fun TilawahBox(
             .clip(RoundedCornerShape(12.dp))
     ) {
         TilawahHeader(
+            readerName = readerName,
             onCloseClick = onCloseClick
         )
 
         TilawahContent(
+            surahName = surahName,
+            currentAyahNumber = currentAyahNumber,
             isPlaying = isPlaying,
+            isLoading = isLoading,
+            repeatCount = repeatCount,
+            isContinuous = isContinuous,
             onRecitersClick = onRecitersClick,
             onBackwardClick = onBackwardClick,
             onPlayPauseClick = onPlayPauseClick,
             onForwardClick = onForwardClick,
             onRepeatClick = onRepeatClick,
-            onPlaylistClick = onPlaylistClick,
+            onContinuousClick = onContinuousClick,
         )
     }
 }
+
 @Composable
 private fun TilawahHeader(
+    readerName: String,
     onCloseClick: () -> Unit
 ) {
     Row(
@@ -195,7 +94,7 @@ private fun TilawahHeader(
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
-            text = "Maytham Al-Tammar",
+            text = readerName,
             color = Theme.color.primary.shadePrimary,
             style = Theme.textStyle.label.small,
             maxLines = 1,
@@ -212,16 +111,29 @@ private fun TilawahHeader(
         )
     }
 }
+
 @Composable
 private fun TilawahContent(
+    surahName: String,
+    currentAyahNumber: Int,
     isPlaying: Boolean,
+    isLoading: Boolean,
+    repeatCount: Int,
+    isContinuous: Boolean,
     onRecitersClick: () -> Unit,
     onBackwardClick: () -> Unit,
     onPlayPauseClick: () -> Unit,
     onForwardClick: () -> Unit,
     onRepeatClick: () -> Unit,
-    onPlaylistClick: () -> Unit,
+    onContinuousClick: () -> Unit,
 ) {
+    val appLocale = LocalAppLocale.current
+    val formattedAyahText = localizedString(
+        id = R.string.surah_ayah_format,
+        surahName,
+        currentAyahNumber.toString().toLocalizedDigits(appLocale)
+    )
+
     Column(
         modifier = Modifier
             .fillMaxWidth()
@@ -245,9 +157,8 @@ private fun TilawahContent(
             .padding(horizontal = 8.dp, vertical = 12.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
         Text(
-            text = "Al-Baarah: 4 aya",
+            text = formattedAyahText,
             color = Theme.color.secondary.shadeSecondary,
             style = Theme.textStyle.label.small,
             maxLines = 1,
@@ -256,24 +167,31 @@ private fun TilawahContent(
 
         ControlsRow(
             isPlaying = isPlaying,
-            onRecitersClick,
-            onBackwardClick,
-            onPlayPauseClick,
-            onForwardClick,
-            onRepeatClick,
-            onPlaylistClick
+            isLoading = isLoading,
+            repeatCount = repeatCount,
+            isContinuous = isContinuous,
+            onRecitersClick = onRecitersClick,
+            onBackwardClick = onBackwardClick,
+            onPlayPauseClick = onPlayPauseClick,
+            onForwardClick = onForwardClick,
+            onRepeatClick = onRepeatClick,
+            onContinuousClick = onContinuousClick
         )
     }
 }
+
 @Composable
 private fun ControlsRow(
     isPlaying: Boolean,
+    isLoading: Boolean,
+    repeatCount: Int,
+    isContinuous: Boolean,
     onRecitersClick: () -> Unit,
     onBackwardClick: () -> Unit,
     onPlayPauseClick: () -> Unit,
     onForwardClick: () -> Unit,
     onRepeatClick: () -> Unit,
-    onPlaylistClick: () -> Unit,
+    onContinuousClick: () -> Unit,
 ) {
     val isRtl = LocalLayoutDirection.current == LayoutDirection.Rtl
 
@@ -282,7 +200,6 @@ private fun ControlsRow(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.spacedBy(10.dp)
     ) {
-
         Icon(
             painter = painterResource(R.drawable.ic_user_list),
             contentDescription = null,
@@ -293,6 +210,7 @@ private fun ControlsRow(
         PlayerControls(
             isRtl = isRtl,
             isPlaying = isPlaying,
+            isLoading = isLoading,
             onBackwardClick = onBackwardClick,
             onPlayPauseClick = onPlayPauseClick,
             onForwardClick = onForwardClick
@@ -301,24 +219,26 @@ private fun ControlsRow(
         Icon(
             painter = painterResource(R.drawable.ic_repeat_one_01),
             contentDescription = null,
-            tint = Theme.color.primary.primary,
+            tint = if (repeatCount > 0) Theme.color.primary.primary else Theme.color.semantic.shadeTertiary,
             modifier = Modifier.clickable { onRepeatClick() }
         )
 
         Icon(
             modifier = Modifier
                 .graphicsLayer { scaleX = if (isRtl) -1f else 1f }
-                .clickable { onPlaylistClick() },
+                .clickable { onContinuousClick() },
             painter = painterResource(R.drawable.ic_curvy_right_direction),
             contentDescription = null,
-            tint = Theme.color.primary.primary
+            tint = if (isContinuous) Theme.color.primary.primary else Theme.color.semantic.shadeTertiary
         )
     }
 }
+
 @Composable
 private fun PlayerControls(
     isRtl: Boolean,
     isPlaying: Boolean,
+    isLoading: Boolean,
     onBackwardClick: () -> Unit,
     onPlayPauseClick: () -> Unit,
     onForwardClick: () -> Unit
@@ -329,8 +249,8 @@ private fun PlayerControls(
             .clip(RoundedCornerShape(8.dp))
             .background(Theme.color.surfaces.surface)
             .padding(vertical = 4.dp, horizontal = 12.dp),
+        verticalAlignment = Alignment.CenterVertically
     ) {
-
         Icon(
             modifier = Modifier
                 .graphicsLayer { scaleX = if (isRtl) -1f else 1f }
@@ -340,19 +260,28 @@ private fun PlayerControls(
             tint = Theme.color.primary.primary
         )
 
-        Icon(
+        Box(
             modifier = Modifier
                 .padding(horizontal = 32.dp)
-                .clickable { onPlayPauseClick() },
-            painter = painterResource(
-                if (isPlaying)
-                    R.drawable.ic_pause
-                else
-                    R.drawable.ic_play
-            ),
-            contentDescription = null,
-            tint = Theme.color.primary.primary
-        )
+                .clickable(enabled = !isLoading) { onPlayPauseClick() },
+            contentAlignment = Alignment.Center
+        ) {
+            if (isLoading) {
+                CircularProgressIndicator(
+                    modifier = Modifier.size(20.dp),
+                    color = Theme.color.primary.primary,
+                    strokeWidth = 2.5.dp
+                )
+            } else {
+                Icon(
+                    painter = painterResource(
+                        if (isPlaying) R.drawable.ic_pause else R.drawable.ic_play
+                    ),
+                    contentDescription = null,
+                    tint = Theme.color.primary.primary
+                )
+            }
+        }
 
         Icon(
             modifier = Modifier
@@ -362,12 +291,5 @@ private fun PlayerControls(
             contentDescription = null,
             tint = Theme.color.primary.primary
         )
-    }
-}
-@Preview
-@Composable
-private fun TilawahBoxPreview() {
-    MehrabTheme(isDarkTheme = false) {
-        TilawahBox(isPlaying = false)
     }
 }
