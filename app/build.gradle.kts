eb790da1cc434e894fef4bd8f2dd953f06646ed3
Elsayed Magdy <saikoo1239@gmail.com>
2026-08-29 13:16:30 +0300

    chore: bump version code to 18 and version name to 1.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,8 +47,8 @@ android {
         applicationId = "dev.sayed.mehrabalmomen"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.1.1"
+        versionCode = 18
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
