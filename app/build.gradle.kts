d02f2b26497e0431539a9a672d727590b82709a7
Elsayed Magdy <saikoo1239@gmail.com>
2026-08-31 16:14:50 +0300

    build: increment versionCode to 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,7 +47,7 @@ android {
         applicationId = "dev.sayed.mehrabalmomen"
         minSdk = 26
         targetSdk = 36
-        versionCode = 19
+        versionCode = 20
         versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
